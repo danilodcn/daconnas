bd13fb013aecb8c495ca5fcea23f761dbcadc2fe
Danilo Nascimento <daconnas.dcn@gmail.com>
2021-10-26 15:41:41 -0300

    create home

--- daconnas/urls.py
+++ daconnas/urls.py
@@ -4,9 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from web.views import home_web
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def home(request):
+    return render(request, "core/base.html", context={"title": "Daconnas"})
 
 
 urlpatterns = [
+    path("", home, name="home"),
     path("index", home_web, name="home_url"),
     path('admin/', admin.site.urls),
     # path("web/", include("web.urls")),
